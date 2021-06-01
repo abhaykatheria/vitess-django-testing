e230b8f616fd293bfe8e6c9f70937347ef3752fe
abhaykatheria <abhay.katheria1998@gmail.com>
2021-06-01 22:03:56 +0530

    coooool

--- vitessDjango/testApp/models.py
+++ vitessDjango/testApp/models.py
@@ -9,8 +9,11 @@ class Person(models.Model):
 
     def __str__(self):
         return self.first_name
-    
+
+
 class Event(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
     title = models.CharField(max_length=150)
     start_date = models.DateField()
     start_date_time = models.DateTimeField()
@@ -20,12 +23,22 @@ class Event(models.Model):
     slug = models.SlugField()
     no_of_attendees = models.IntegerField()
     no_of_views = models.BigIntegerField()
-    website = models.URLField()
+    website = models.URLField(null=True)
     start_time = models.TimeField()
     summary = models.TextField()
     brochure = models.FileField()
-    rating = models.FloatField()
+    rating = models.FloatField(blank=True, default="5.0")
     is_accepting_registration = models.BooleanField()
+    SHIRT_SIZES = (
+        ("S", "Small"),
+        ("M", "Medium"),
+        ("L", "Large"),
+    )
+    t_size = models.CharField(max_length=20, choices=SHIRT_SIZES)
+
+    def __str__(self):
+        return self.title
+
 
 class Server(models.Model):
     uuid = models.UUIDField()
@@ -35,12 +48,8 @@ class Server(models.Model):
     freq_in_ghz = models.SmallIntegerField()
     no_of_threads = models.PositiveIntegerField()
     binary_sig = models.BinaryField()
-    cache_size = models.DecimalField()
-    
+    cache_size = models.DecimalField(max_digits=100, decimal_places=10)
+    name = models.CharField(max_length=100)
 
-
-
-
-
-
-
+    def __str__(self):
+        return self.name
